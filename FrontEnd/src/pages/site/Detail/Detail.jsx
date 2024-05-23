da1d1cf5ef4da99e137a579b8f2a0ab498aa0ddb
Yusif-Bedel <yusifbedel01@gmail.com>
2024-05-23 20:20:57 +0400

    son task :(((

--- FrontEnd/src/pages/site/Detail/Detail.jsx
+++ FrontEnd/src/pages/site/Detail/Detail.jsx
@@ -1,14 +1,17 @@
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-
+import MainContext from '../../../context/context'
 const Detail = () => {
   const {id}=useParams()
+  const {data}=useContext(MainContext)
   const [itemData,setItemData]=useState([])
+  const [wishlist,setWishlist]=useState(localStorage.getItem("wishlist")?JSON.parse(localStorage.getItem("wishlist")):[])
   const [basket,setBasket]=useState(localStorage.getItem("basket")?JSON.parse(localStorage.getItem("basket")):[])
   useEffect(()=>{
     localStorage.setItem("basket",JSON.stringify(basket))
-  },[basket])
+    localStorage.setItem("wishlist",JSON.stringify(wishlist))
+  },[basket,wishlist])
   useEffect(()=>{
     axios.get(`http://localhost:4404/products/${id}`).then((res)=>{
       setItemData(res.data)
@@ -27,6 +30,18 @@ const Detail = () => {
       setBasket([...basket])
     }
   }
+  const wishHandler=(title)=>{
+    let wishItem=wishlist.find((x)=>x.title==title)
+    if(wishItem){
+      setWishlist([...wishlist.filter((x)=>x.title!=title)])
+      alert("item removed from wishlist")
+    }
+    else{
+      let target=data.find((x)=>x.title==title)
+      setWishlist([...wishlist,target])
+      alert("item added to wishlist")
+    }
+  }
   return (
     <div style={{display:"flex",justifyContent:"space-evenly",alignItems:"center"}} className="detail">
       <img width={"400px"} src={itemData.image} alt="" />
@@ -37,6 +52,9 @@ const Detail = () => {
         <button className='btn btn-warning' onClick={()=>{
           addToBasket(itemData.title)
         }}>Add to Basket</button>
+        <button className='btn btn-secondary' onClick={()=>{
+          wishHandler(itemData.title)
+        }}><i class="fa-solid fa-heart"></i></button>
       </div>
     </div>
   )
--- FrontEnd/src/pages/site/Wishlist/Wishlist.jsx
+++ FrontEnd/src/pages/site/Wishlist/Wishlist.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Wishlist = () => {
+  const [wishlist, setWishlist] = useState(
+    localStorage.getItem("wishlist")
+      ? JSON.parse(localStorage.getItem("wishlist"))
+      : []
+  );
+  useEffect(() => {
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  }, [wishlist]);
+  const wishHandler=(title)=>{
+    let wishItem=wishlist.find((x)=>x.title==title)
+    if(wishItem){
+      setWishlist([...wishlist.filter((x)=>x.title!=title)])
+      alert("item removed from wishlist")
+    }
+    else{
+      let target=data.find((x)=>x.title==title)
+      setWishlist([...wishlist,target])
+      alert("item added to wishlist")
+    }
+  }
   return (
     <div>
-      
-    </div>
-  )
-}
+      <table class="table">
+        <thead>
+          <tr>
 
-export default Wishlist
+            <th scope="col">Image</th>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+            <th scope="col">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            wishlist.map((item,index)=>{
+              console.log(item)
+              return(<tr key={index} >
+            <td><img width={"60px"} src={item.image} alt=""/></td>
+            <td>{item.title}</td>
+            <td>{item.price}</td>
+            <td><button className="btn btn-danger" onClick={()=>{
+              wishHandler(item.title)
+            }}>Remove</button></td>
+          </tr>)
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Wishlist;
